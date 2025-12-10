4e1c97f8957dd97a3a57a001398b7a39c95cf457
Yuttana.H <yhumda@gmail.com>
2025-12-10 15:56:26 +0700

    submit lab 2

---
+++ yuttana_widgets_exercises/lib/main_quote_card.dart
@@ -0,0 +1,64 @@
+/// @author Yuttana Hoomda
+/// @version 1.0
+/// @date 2025-12-10
+/// widget quote card that use expanded make img full width
+/// this lab will show picture of quote and the author
+library;
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const QuoteCard());
+}
+
+class QuoteCard extends StatelessWidget {
+  const QuoteCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: .fromSeed(seedColor: Colors.blue),
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.blue[200],
+          title: Text("My Favorite Quote"),
+        ),
+        body: Padding(
+          padding: EdgeInsetsGeometry.all(10),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Expanded(child: Image.asset('assets/images.jpg', height: 500, fit: BoxFit.cover,)),
+              Expanded(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                        'William',
+                        style: TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.blueAccent
+                        )),
+                    const SizedBox(width: 20),
+                    const Text(
+                        'Shakespeare',
+                        style: TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.blueAccent
+                        )),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
